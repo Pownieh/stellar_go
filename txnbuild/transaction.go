8967e988285c8837a4bae36c81da5487a5a4d85f
Alexander Munch-Hansen <amunchhansen@blockdaemon.com>
2023-12-04 13:04:25 +0100

    set envelope

--- txnbuild/transaction.go
+++ txnbuild/transaction.go
@@ -295,6 +295,10 @@ func (t *Transaction) GetEnvelope() xdr.TransactionEnvelope {
 	return t.envelope
 }
 
+func (t *Transaction) SetEnvelope(env xdr.TransactionEnvelope) {
+	t.envelope = env
+}
+
 // Sign returns a new Transaction instance which extends the current instance
 // with additional signatures derived from the given list of keypair instances.
 func (t *Transaction) Sign(network string, kps ...*keypair.Full) (*Transaction, error) {
